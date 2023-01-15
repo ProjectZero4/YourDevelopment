36896a698f8f8bc676c3ebbb7d6692476eec05f8
Josh Capener <josh@joshcapener.co.uk>
2023-01-15 19:58:03 +0000

    Fix for ssl certs being CA=True

--- .setup/scripts/certificates.py
+++ .setup/scripts/certificates.py
@@ -5,4 +5,4 @@ currentDir = os.path.dirname(os.path.realpath(__file__)) + "/"
 
 def createCertificate(cn):
     ca = CertificateAuthority(ca_storage=currentDir+"../../nginx/ssl", common_name='Your Development')
-    ca.issue_certificate(hostname=cn)
+    ca.issue_certificate(hostname=cn, ca=False)
